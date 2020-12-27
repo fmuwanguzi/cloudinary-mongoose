c87e6ea329e0a5231090db5c67ad9f906f7fc96d
Felix Muwanguzi <felixm@Felixs-MacBook-Air.local>
2020-12-27 11:25:22 -0500

    [model and dotenv] set up

--- models/index.js
+++ models/index.js
@@ -5,7 +5,8 @@ const connectDB = async () => {
         const con = await mongoose.connect(process.env.MONGO_URI,{
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useFindAndModify: true 
+            useFindAndModify: true,
+            useCreateIndex: true
         })
         console.log(`MongoDb Connected : ${con.connection.host}`);
     }catch (error) {
