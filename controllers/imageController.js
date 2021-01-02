198b44a100f56ae553f7cd5a7038b6f1508c70c3
Felix Muwanguzi <felixm@Felixs-MacBook-Air.local>
2021-01-02 16:07:43 -0500

    [GET AND DELETE] routes are working

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -5,7 +5,7 @@ const upload = require('../config/multer');
 const Image = require('../models/Image');
 require("dotenv").config();
 
-//may change the 'image' after upload.single because it may become confusing.
+//Post route to send images to cloudinary
 
 router.post('/', upload.single('image'), async (req , res) => {
     //console.log(cloudinary);
@@ -16,7 +16,7 @@ router.post('/', upload.single('image'), async (req , res) => {
         //creating a new image which is an object can be whatever your models is 
         const image = new Image({
             name: req.body.name,
-            description: myImage.secure_url,
+            picture: myImage.secure_url,
             cloudinary_id: myImage.public_id, 
         });
         await image.save();
@@ -26,14 +26,42 @@ router.post('/', upload.single('image'), async (req , res) => {
     }
 });
 
+//Get route to display all images we have sent to cloudinary 
+
+router.get('/', async (req, res) => {
+    try {
+        const image = await Image.find();
+        res.json(image);
+    }catch(error){
+        console.log(error);
+    }
+});
+
+//get each image by id
+router.get('/:id', async (req, res) => {
+    try {
+        const image = await Image.findById(req.params.id)
+        res.json(image);
+    }catch(error){
+        console.log(error);
+    }
+});
+
+//Delete route
+
+router.delete('/:id' , async (req, res) => {
+    try {
+        //Find image by the ID
+        const image = await Image.findById(req.params.id);
+        //Deleting the image from cloudinary
+        await cloudinary.uploader.destroy(image.cloudinary_id);
+        //Delete the image from the database
+        await image.remove();
+        res.json(image);
+    } catch (error){
+        console.log(error);
+    }
+});
+
 module.exports = router; 
 
-
-
-
-
-
-
-
-
-module.exports = router;
--- models/Image.js
+++ models/Image.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const imageSchema = new mongoose.Schema({
     name:String,
-    description: String,
+    picture: String,
     cloudinary_id: String
 })
 
